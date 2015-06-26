68d7d59cb527217c5eb7ec26775bb800bf641032
Filipe Mendes Mariz <fmm4@cin.ufpe.br>
2015-06-26 02:26:47 -0300

    Erro quando dei merge.

--- test/cucumber/steps/ListResidueGeneratorSteps.groovy
+++ test/cucumber/steps/ListResidueGeneratorSteps.groovy
@@ -1,45 +1,66 @@
+package steps
 
+import geb.Page
+import pages.ResidueGeneratorListPage
+import pages.ResidueGeneratorShowPage
 import residueGenerator.ResidueGenerator
-import residueGeneratorTestDataAndOperations
-import static cucumber.runtime.groovy.EN.*
+import static cucumber.api.groovy.EN.*
+//CONTROLLER TESTS
 
-
-Given(~'the system has a Gerador de Residuo named "([^"]*)" in it$') {String title->
-    residueGeneratorTestDataAndOperations.createResidueGenerator(title,"ufpe")
-    assert ResidueGenerator.findByNameGenerator(title) !=null
+Given(~'^the system has a Residue Generator at "([^"]*)" in it$'){String address ->
+    GeneratorTestDataAndOperations.createGeneratorWithDailyMeal(address,0)
+    generator = ResidueGenerator.findByAddressGenerator(address)
+    assert generator != null
 }
 
-When (~'^I select the list geradores option$') { ->
-    residueGen = ResidueGenerator.findAll()
-    assert residueGen != null
+When(~'^the system list all existing Residue Generators$'){->
+    def allResidue = ResidueGenerator.list()
 }
 
-Then(~'^a list of results containing "([^"]*)" appears$') { String title ->
-    residueGen = ResidueGenerator.findAll()
-    assert residueGeneratorTestDataAndOperations.containResidueGenerator(title)
+Then(~'^the system list contains "([^"]*)"$'){String address->
+    def allResidue = ResidueGenerator.list()
+    def foundResidue = ResidueGenerator.findByAddressGenerator(address)
+    allResidue.find{foundResidue}
 }
 
-Given(~'the system has a Gerador de Residuo named "([^"]*)" with volume "([^"]*)"$') { String title, float volume ->
-    residueGeneratorTestDataAndOperations.createResidueGenerator(title,"ufpe")
-    assert ResidueGenerator.findByNameGenerator(title) != null
+When(~'^I create a list with all Residue Generators$'){->
+    def allResidue = ResidueGenerator.list()
 }
 
-When (~'^I sort the list content by volume$') {
-    residueGeneratorSorted = ResidueGenerator.listOrderByVolume(order: "asc")
-    assert residueGeneratorTestDataAndOperations.isSorted(residueGeneratorSorted, "volume")
+Then(~'^a list of results containing a Residue Generator at "([^"]*)" appears$'){String address ->
+    def residueGen = ResidueGenerator.list()
+    def wantedResGen = GeneratorTestDataAndOperations.findGeneratorByAddress(address)
+    assert residueGen.contains(wantedResGen)
 }
 
-Then(~'^a list of results containing "([^"]*)" before "([^"]*)" appears$') { String former, String latter ->
-    //TODO (Como fazer?)
+Given(~'^the system has a Residue Generator at "([^"]*)" with average daily meal of "([^"]*)"$'){String address, int avgDaily ->
+    GeneratorTestDataAndOperations.createGeneratorWithDailyMeal(address, avgDaily)
+    generator = ResidueGenerator.findByAddressGenerator(address)
+    assert generator != null
 }
 
-
-Given(~'I am on the Residue Generators page$') {
-    to ListaGeradorDeResiduoPage
-    at ListaGeradorDeResiduoPage
+When(~'^I sort the list content by average daily meals$'){->
+    residueGeneratorSorted = ResidueGenerator.listOrderByAverageDailyMeals(order: "desc");
+    assert GeneratorTestDataAndOperations.isSorted(residueGeneratorSorted);
 }
 
-Given(~'I am on the Index page$'){
+Then(~'^the order of the system list have been modified to have address "([^"]*)" before "([^"]*)"$') { String before, String after ->
+    residueGeneratorSorted = ResidueGenerator.listOrderByAverageDailyMeals(order: "desc");
+    generatorFirst = ResidueGenerator.findByAddressGenerator(before);
+    generatorLatter = ResidueGenerator.findByAddressGenerator(after);
+    assert residueGeneratorSorted.indexOf(generatorFirst) < residueGeneratorSorted.indexOf(generatorLatter)
+}
+
+//GUI
+
+Given(~'^I am at the Residue Generator Show page$'){ ->
+    to ResidueGeneratorShowPage
+    at ResidueGeneratorShowPage
+}
+
+When(~'^I go to the List Residue Generators page$'){->
+    page.selectListResidueGenerators()
+}
 
 Then(~'^I am at the Residue Generator List page$'){->
     to ResidueGeneratorListPage
@@ -51,27 +72,25 @@ Given(~'^I am at the Residue Generators List page$'){->
     at ResidueGeneratorListPage
 }
 
-Then(~'I am now on the Residue Generators page$') {
-    at ListaGeradorDeResiduoPage
+When(~'^I sort by Average Daily Meal$'){ ->
+    page.selectSortByAverageDailyMeal()
 }
 
-When(~'I click on the Sort by Volume button$'){
-    at ListaGeradorDeResiduoPage
-    ListaGeradorDeResiduoPage.sortByVolume()
+Then(~'^the displayed list is sorted by Average Daily Meal$'){ ->
+
 }
 
-Then(~'the list is sorted by volume$'){->
-    residueGeneratorSorted = ResidueGenerator.listOrderByVolume(order: "asc")
-    assert residueGeneratorTestDataAndOperations.isSorted(residueGeneratorSorted, "volume")
-    //TODO (Seria da mesma forma como o 'When?')
+Given(~'^there are no Residue Generator in the system$') { ->
+    residueGen = ResidueGenerator.findAll()
+    assert residueGen == null
 }
 
-Given(~'there are no Gerador de Residuo in the system$'){->
-    residueGenerators = ResidueGenerator.findAll()
-    assert ResidueGenerator == null
+Then(~'^an error message saying there are no stored residue generator should appear$'){->
+    def hasMessage = page.hasMessage()
+    assert hasMessage != null
 }
 
-Then(~'an error message "([^"]*)" appears$') { String message ->
-    assert (page.readFlashMessage() !=null)
-    //Nao sei se esta correto.
+Then(~'^I am at the Create Residue Generator page$'){
+    to GeneratorCreatePage
+    at GeneratorCreatePage
 }
